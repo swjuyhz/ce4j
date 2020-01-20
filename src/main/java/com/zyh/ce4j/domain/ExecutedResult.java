ec5cef0040a61da69fce9da1fca93117b080d15a
river-rock <1143875182@qq.com>
2020-01-20 16:46:24 +0800

    delete desc
    delete desc

--- src/main/java/com/zyh/ce4j/domain/ExecutedResult.java
+++ src/main/java/com/zyh/ce4j/domain/ExecutedResult.java
@@ -1,17 +1,3 @@
-/**
- * ============================================================================
- * = COPYRIGHT
- * PAX Computer Technology(Shenzhen) CO., LTD PROPRIETARY INFORMATION
- * This software is supplied under the terms of a license agreement or nondisclosure
- * agreement with PAX Computer Technology(Shenzhen) CO., LTD and may not be copied or
- * disclosed except in accordance with the terms in that agreement.
- * Copyright (C) 2018/11/5-? PAX Computer Technology(Shenzhen) CO., LTD All rights reserved.
- * Description:
- * Revision History:
- * Date                         Author                    Action
- * 2018/11/5 15:13           liming                   Result
- * ============================================================================
- */
 package com.zyh.ce4j.domain;
 
 public class ExecutedResult {
